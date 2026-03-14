747a87a67634efca9a9c7fa7964197d25dd96ba2
Vladyslav Cherniuk <vlad26v03@gmail.com>
2026-03-14 16:00:19 +0200

    Add installation command script

---
+++ start.sh
@@ -0,0 +1,11 @@
+export COMPOSE_PROJECT_NAME=moodle34
+export MOODLE_DOCKER_WEB_PORT=80
+export MOODLE_DOCKER_WEB_HOST=localhost
+
+bin/moodle-docker-compose exec webserver php admin/tool/behat/cli/init.php
+bin/moodle-docker-compose exec -u www-data webserver php admin/tool/behat/cli/run.php --tags=@auth_manual
+
+bin/moodle-docker-compose exec webserver php admin/tool/phpunit/cli/init.php
+bin/moodle-docker-compose exec webserver vendor/bin/phpunit auth/manual/tests/manual_test.php
+
+bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="Docker moodle" --shortname="docker_moodle" --summary="Docker moodle site" --adminpass="test" --adminemail="admin@example.com"

477e3e73da5999ff2c49e6c8681dc469e583fee2
Vladyslav Cherniuk <vlad26v03@gmail.com>
2026-03-15 01:07:42 +0200

    Add prod deployment script

---
+++ start-prod.sh
@@ -0,0 +1,19 @@
+export COMPOSE_PROJECT_NAME=moodle34
+export MOODLE_DOCKER_WEB_PORT=0
+#export MOODLE_DOCKER_WEB_HOST=
+export MOODLE_DOCKER_WWWROOT=./moodle
+export MOODLE_DOCKER_DB=pgsql
+
+cp config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+
+bin/moodle-docker-compose down
+bin/moodle-docker-compose up -d
+bin/moodle-docker-wait-for-db
+
+bin/moodle-docker-compose exec webserver php admin/tool/behat/cli/init.php
+bin/moodle-docker-compose exec -u www-data webserver php admin/tool/behat/cli/run.php --tags=@auth_manual
+
+bin/moodle-docker-compose exec webserver php admin/tool/phpunit/cli/init.php
+bin/moodle-docker-compose exec webserver vendor/bin/phpunit auth/manual/tests/manual_test.php
+
+bin/moodle-docker-compose exec webserver php admin/cli/install_database.php --agree-license --fullname="Docker moodle" --shortname="docker_moodle" --summary="Docker moodle site" --adminpass="test" --adminemail="admin@example.com"

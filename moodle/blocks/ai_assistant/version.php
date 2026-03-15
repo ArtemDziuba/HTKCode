e6f9d42fd1650fa06f47835673ea276735b91565
victor1dyminskyi <victor.dyminskyi@gmail.com>
2026-03-15 12:00:37 +0200

    added a bot into edditing of courses

--- moodle/blocks/ai_assistant/version.php
+++ moodle/blocks/ai_assistant/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2025031400;
+$plugin->version   = 2025031401;
 $plugin->requires  = 2022041900;
 $plugin->component = 'block_ai_assistant';
 $plugin->cron = 0;
